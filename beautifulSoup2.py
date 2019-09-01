9c95b8073c88a632e91c6c3353f702c261008a59
Aiden Park <dev.parkjangwon@gmail.com>
2019-09-01 18:58:57 +0900

    add png, jfif

--- beautifulSoup2.py
+++ beautifulSoup2.py
@@ -1,4 +1,4 @@
-import os, pathlib, re, shutil
+import os, pathlib, re, shutil, base64
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
@@ -38,6 +38,17 @@ try :
     # enumerate makes and returns the tuple that has sequence.
     for i, _img in enumerate(_imgs) :
         _srcUrl = _img.get("src")
+        # print(_srcUrl)
+
+        # base64 encoding image (png)
+        _match = re.match(r"^data:image/jpeg;base64,(?P<_imgData>.+)$", _srcUrl)
+        if _match is not None :
+            #_match.group(0)            # The entire match
+            #_match.group(1)            # The first parenthesized subgroup.
+            #_match.group("_imgData")   # by group name
+            with open("{}\img_{}.png".format(_trgtDir, i), "wb") as _fh:
+                _fh.write(base64.decodebytes(_match.group("_imgData")))
+            continue
 
         # https://s.pstatic.net/static/www/mobile/edit/2019/0829/mobile_180126994547.jpg?345345
         # //s.pstatic.net/static/www/mobile/edit/2019/0829/mobile_180126994547.jpg?345345
@@ -46,22 +57,39 @@ try :
         # ./mobile_180126994547.jpg?345345
         # mobile_180126994547.jpg?345345
         # finadall returns tuple [('', '', '', '')]
-        _groups         = re.findall(r"^(http[s]?)?(?::)?(?://)?(\w+\.?\w+\.\w+)?(.*/)?(\w+\.[^?]+).*$", _srcUrl)[0]
-        _schema         = _request.type if not _groups[0] else _groups[0]
-        _domain         = _request.host if not _groups[1] else _groups[1]
-        _middlePath     = _groups[2]
-        _fileName       = _groups[3]
+        # 추출하는 영역()이 하나면 단순 string 구성 list고
+        # 추출하는 영역()이 여러개면 tuple 로 구성된 list 반환한다.
+        _groups = re.findall(r"^(http[s]?)?(?::)?(?://)?(\w+\.?\w+\.\w+)?(.*/)?(\w+\.[^?]+).*$", _srcUrl)
 
-        # validate filename one more time
-        _match = re.match(r"^\w+\.\w+$", _fileName)
-        if _match is None :
-            continue
+        # pattern 맞지 않는 url (ex google)
+        # src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXEf6i5MVKM_X6LWBqOiYtJVhoz9uUtIvAhRCQ4W0FeeclXyfMx15rbT4"
+        if len(_groups) == 0 :
+            _srcAbsUrl      = _srcUrl
+            _fileName       = "img_{}.{}".format(i, "jfif")
 
-        _srcAbsUrl      = "{}://{}{}{}".format(_schema, _domain, _middlePath, _fileName)
+        # normal pattern
+        else :
+            _group          = _groups[0]
+            _schema         = _request.type if not _group[0] else _group[0]
+            _domain         = _request.host if not _group[1] else _group[1]
+            _middlePath     = _group[2]
+            _fileName       = _group[3]
+
+            # validate filename one more time
+            _match = re.match(r"^\w+\.\w+$", _fileName)
+            if _match is None :
+                continue
+
+            _srcAbsUrl      = "{}://{}{}{}".format(_schema, _domain, _middlePath, _fileName)
 
         print(i, _fileName, _srcUrl, _srcAbsUrl)
-        urllib.request.urlretrieve(_srcAbsUrl, "{}\{}".format(_trgtDir, _fileName))    # 역슬레쉬 하나로 해도 되고 두개로 해도 된다.
-        _counts += 1
+
+        try :
+            urllib.request.urlretrieve(_srcAbsUrl, "{}\{}".format(_trgtDir, _fileName))    # 역슬레쉬 하나로 해도 되고 두개로 해도 된다.
+            _counts += 1
+        except Exception as e :
+            print("Exception :", e)
+            continue
 
 except Exception as e :
     print("Exception :", e)
