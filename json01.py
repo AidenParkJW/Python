8defc243a835e35d1624bdac25e29f7f40e6f74d
AidenParkJW <54151123+AidenParkJW@users.noreply.github.com>
2019-09-02 15:38:50 +0900

    Add files via upload

---
+++ json01.py
@@ -0,0 +1,21 @@
+import json
+
+data = '''{"name":"Chuck", "phone":{"type":"tntl", "number":"+1 734 303 4456"}, "email" : {"hide":"yes"}}'''
+
+info = json.loads(data)
+print(type(info))
+print("Name :", info["name"])
+print("Hide :", info["email"]["hide"])
+print()
+
+
+data = '''[{"id":"001", "x":"2", "name":"Chuck"}, {"id":"009", "x":"7", "name":"Chuck"}]'''
+
+info = json.loads(data)
+print(type(info))
+
+for item in info :
+    print("Name :", item["name"])
+    print("id :", item["id"])
+    print("X :", item["x"])
+    print()
