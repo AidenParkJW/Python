8237894e440eea417cee8ce9c0422e15a557e1cf
AidenParkJW <54151123+AidenParkJW@users.noreply.github.com>
2019-09-03 11:30:52 +0900

    Update xml01.py

--- xml01.py
+++ xml01.py
@@ -1,6 +1,10 @@
 import xml.etree.ElementTree as ET
 
-data = "<person><name>Chunk</name><pone type='intl'>+1 734 303 4456</pone><email hide='yes'/></person>"
+data = '''<person>
+    <name>Chunk</name>
+    <pone type="intl">+1 734 303 4456</pone>
+    <email hide="yes"/>
+</person>'''
 
 print(data)
 tree = ET.fromstring(data)
@@ -9,7 +13,18 @@ print("Name :", tree.find("name").text)
 print("Attr :", tree.find("email").get("hide"))
 print()
 
-data = "<stuff><users><user x='2'><id>001</id><name>Chuck</name></user><user x='7'><id>009</id><name>Brent</name></user></users></stuff>"
+data = '''<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>'''
 
 print(data)
 tree = ET.fromstring(data)
