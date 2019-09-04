5f3cff658c2b53f739591e50dede1c7014fceb2d
Aiden Park <dev.parkjangwon@gmail.com>
2019-09-05 03:26:26 +0900

    .

--- geojson.py
+++ geojson.py
@@ -36,6 +36,8 @@ while True :
         print(data)
         continue
 
+    print(json.dumps(js, indent=4))
+
     lat = js["results"][0]["geometry"]["location"]["lat"]
     lng = js["results"][0]["geometry"]["location"]["lng"]
     print("lat : {}, lng : {}".format(lat, lng))
