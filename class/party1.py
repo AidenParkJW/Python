74fce79b466af137a361811cd6c5ffccee56b5be
Aiden Park <dev.parkjangwon@gmail.com>
2019-09-06 01:43:26 +0900

    add class exercise

--- class/party1.py
+++ class/party1.py
@@ -1,18 +1,13 @@
 class PartyAnimal :
-	x = 0
+    x = 0
 
     def party(self) :
         self.x += 1
         print("So far :", self.x)
 
 
-# >>> an = PartyAnimal()
-# >>> an.party()
-# So far : 1
-# >>> an.party()
-# So far : 2
-# >>> PartyAnimal.party(an)
-# So far : 3
-# >>> an.party()
-# So far : 4
-# >>> 
+an = PartyAnimal()
+an.party()              # So far : 1
+an.party()              # So far : 2
+PartyAnimal.party(an)   # So far : 3
+an.party()              # So far : 4
