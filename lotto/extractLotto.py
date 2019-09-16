33cc6dd9ce07f1ccdbeb1f6fd314c2d86165e074
AidenParkJW <54151123+AidenParkJW@users.noreply.github.com>
2019-09-16 15:07:32 +0900

    Update extractLotto.py

--- lotto/extractLotto.py
+++ lotto/extractLotto.py
@@ -135,7 +135,7 @@ print("--" * 50)
 # print data
 rows = csr.fetchall()
 for row in rows:
-    print("{:5} {:5} {:5} {:5} {:5} {:5} {:5} {:10} {:20} {:20}".format(row["0"], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
+    print("{:5} {:5} {:5} {:5} {:5} {:5} {:5} {:10} {:20} {:20}".format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
 
 csr.close()
 con.close()
