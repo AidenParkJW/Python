c5c2f8e6a24951146c522729ccf7c8b2a7066eb5
AidenParkJW <54151123+AidenParkJW@users.noreply.github.com>
2019-09-16 13:09:15 +0900

    Add files via upload

--- Network/json/json01.py
+++ Network/json/json01.py
@@ -3,7 +3,7 @@ import json
 data = '''{"name":"Chuck", "phone":{"type":"tntl", "number":"+1 734 303 4456"}, "email" : {"hide":"yes"}}'''
 
 info = json.loads(data)
-print(type(info))
+print(type(info))   # <class 'dict'>
 print("Name :", info["name"])
 print("Hide :", info["email"]["hide"])
 print()
@@ -12,7 +12,7 @@ print()
 data = '''[{"id":"001", "x":"2", "name":"Chuck"}, {"id":"009", "x":"7", "name":"Chuck"}]'''
 
 info = json.loads(data)
-print(type(info))
+print(type(info))   # <class 'list'>
 print("Length :", len(info))
 
 for item in info :
